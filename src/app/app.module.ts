b6ec31dac5003fbdb984be9ad47d0b567a851488
dvolkhonskiy <dvolkhonskiy@gmail.com>
2019-05-26 19:52:40 +0300

    articles and dashboard components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import {RouterModule, Routes} from '@angular/router';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ArticlesComponent } from './articles/articles.component';
+
+const appRoutes: Routes = [
+  {path: 'dashboard', component: DashboardComponent},
+  {path: 'articles', component: ArticlesComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    ArticlesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
