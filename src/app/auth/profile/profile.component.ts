ffad54d2fc792d8a7160c43ba6800fa84751d898
dvolkhonskiy <dvolkhonskiy@gmail.com>
2019-08-28 11:28:53 +0300

    fix profile-menu

--- src/app/auth/profile/profile.component.ts
+++ src/app/auth/profile/profile.component.ts
@@ -1,8 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Inject, OnInit} from '@angular/core';
 import {APIService} from '../../shared/api.service';
 import {PremiumSubscription} from '../../premium/subscription.model';
 import {AuthService} from '../auth.service';
 import {UserProfile} from '../user.model';
+import {ActivatedRoute} from '@angular/router';
+import {MAT_DIALOG_DATA, MatDialog} from '@angular/material/dialog';
 
 @Component({
   selector: 'app-profile',
@@ -16,12 +18,23 @@ export class ProfileComponent implements OnInit {
 
   isLoading = true;
 
-  constructor(public authService: AuthService, private apiService: APIService) { }
+  constructor(public authService: AuthService,
+              private apiService: APIService,
+              private route: ActivatedRoute,
+              public dialog: MatDialog
+  ) { }
 
   ngOnInit() {
+    this.route.queryParams.subscribe(params => {
+      console.log(params);
+      // if (params.intro === 'true') {
+      //   this.showPremiumAlert();
+      // }
+    });
+
     this.authService.getProfile().subscribe(
       data => {
-        this.profile = data;
+        this.profile = data.profile;
         console.log(data);
       },
       error => {
@@ -41,10 +54,22 @@ export class ProfileComponent implements OnInit {
     );
   }
 
+  showPremiumAlert() {
+    this.dialog.open(DialogDataExampleDialog, {});
+  }
+
   onChange() {
     this.apiService.updateSubscription(this.subscription).subscribe(
       data => console.log(data)
     );
   }
 
+}
+
+@Component({
+  selector: 'welcome_dialog',
+  templateUrl: 'welcome_dialog.html',
+})
+export class DialogDataExampleDialog {
+  constructor() {}
 }
